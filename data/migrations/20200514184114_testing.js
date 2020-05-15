8bd08a7cce07bce4a63447446912b90c3c23dc9e
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-05-14 19:13:49 -0500

    added structure

---
+++ data/migrations/20200514184114_testing.js
@@ -0,0 +1,12 @@
+exports.up = async (knex) => {
+    await knex.schema.createTable("messages", (table) => {
+        table.increments();
+        table.text("title").notNullable();
+        table.text("contents").notNullable();
+        table.timestamps(true, true);
+    });
+};
+
+exports.down = async (knex) => {
+    await knex.schema.dropTableIfExists("messages");
+};
---
+++ data/routers/messages-router.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const db = require("../config");
+
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+    try {
+        const messages = await db("messages").select("*");
+        res.json(messages);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.post("/", async (req, res, next) => {
+    try {
+        const payload = {
+            title: req.body.title,
+            contents: req.body.contents,
+        };
+
+        const [id] = await db("messages").insert(payload);
+        const message = await db("messages").where("id", id).first();
+        res.json(message);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.delete("/:id", async (req, res, next) => {
+    try {
+        await db("messages").where("id", req.params.id).del();
+        res.status(204).end();
+    } catch (err) {
+        next(err);
+    }
+});
